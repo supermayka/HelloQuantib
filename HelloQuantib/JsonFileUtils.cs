00669c1bd4ba2f92dec34409e8d06a6117a252b4
Maya Levin <mayal@uccare.local>
2023-05-30 14:34:23 +0300

    Initial commit

---
+++ HelloQuantib/JsonFileUtils.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace HelloQuantib
+{
+    public static class JsonFileUtils
+    {
+
+        private static readonly JsonSerializerOptions _options =
+            new JsonSerializerOptions() { DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull };
+
+        public static void SimpleWrite(object obj, string fileName)
+        {
+            var jsonString = JsonSerializer.Serialize(obj, _options);
+            File.WriteAllText(fileName, jsonString);
+        }
+    }
+}
---
+++ HelloQuantib/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloQuantib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Quantib!");
+
+            string statusFile = "..\\Status\\status.json";
+            string outputPath = "..\\output";
+            string inputPath = "..\\input";
+            string statusFormat = @"status:{0},description:{1}";
+            string status = "success";
+            string errorMsg = "";
+
+            if (!Directory.Exists(outputPath))
+            {
+                errorMsg = "Output folder is missing";
+                status = "failure";
+            }
+            else if(!Directory.Exists(inputPath))
+            {
+                errorMsg = "Input folder is missing";
+                status = "failure";
+            }
+           
+            status = string.Format(statusFormat, status, errorMsg);
+            JsonFileUtils.SimpleWrite(status, statusFile);
+        }
+
+        
+
+    }
+}
